17d2c6a8386179c03c2123453beefd1de4bcb1f3
Elvis Pfutzenreuter <epxx@epxx.co>
2016-11-01 20:40:05 -0200

    Added missing file

---
+++ android/Biorhythmics/app/src/main/java/epxx/co/biorhythmics/AlarmStartup.java
@@ -0,0 +1,18 @@
+package epxx.co.biorhythmics;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+
+public class AlarmStartup extends BroadcastReceiver {
+    public final String TAG = "Biorhythmics-Boot";
+
+    @Override
+    public void onReceive(Context arg0, Intent arg1) {
+        Log.d(TAG, "boozzed");
+        Intent intent = new Intent(arg0, JavascriptEngine.class);
+        arg0.startService(intent);
+    }
+}
